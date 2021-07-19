102a03cf125e6804b43d385d5efcc8953a457172
LEGOnosuke <50015337+LEGOnosuke@users.noreply.github.com>
2021-07-19 11:56:26 +0900

    None

---
+++ header.php
@@ -0,0 +1,25 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark role-navgation d-print">
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#kirikae" aria-label="navi">
+    <span class="navbar-toggler-icon">
+    </span>
+  </button>
+  <div class="collapse navbar-collapse" id="kirikae">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="home.html">現在の空き情報
+        </a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="zisaku-print.html">Contact us
+        </a>
+      </li>
+    </ul>
+  </div>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <span class="navbar-text ml-auto">市営体育館の開放種目をリアルタイムに表示するサイト
+    </span>
+  </div>
+  <a class="navbar-brand" href="home.html">
+    <img src="img/pin_icon.png" alt="" width="30" height="30">GymFinder
+  </a>
+</nav>
---
+++ js/DisplayDate.js
@@ -0,0 +1,3 @@
+const d = new Date();
+let text = '<h1>'+d.toDateString()+d.toTimeString()+'</h1>'
+document.write(text);
